d202f0c8841c5cdddd0d9d9d92e574a563cc0151
Achi Chen <achi.c@mig.me>
2016-02-18 19:22:56 +0800

    Remove unused code

--- Github/view_file_on_github.sh
+++ Github/view_file_on_github.sh
@@ -19,11 +19,4 @@ function get_github_base_url() {
     echo $giturl
 }
 
-function get_md5sum() {
-    # I installed md5sha1sum on Mac OS X by using the command "brew install md5sha1sum"
-    echo -n $1 | /usr/local/bin/md5sum | cut -d ' ' -f 1
-}
-
-#https://github.com/migme/adhoc-scripts/blob/4d1460bbbf7e8e57700b46aafac654479a8dea89/unity/manual/vpn/tinc/README.md
-
 open "$(get_github_base_url)blob/$1/$2"
